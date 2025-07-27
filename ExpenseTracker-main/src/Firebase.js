d68b5136a7d13e52060d771bd6067341ff109ac1
Nilim <nilimgithub45@gmail.com>
2025-07-27 12:32:25 +0530

    Update Firebase.js

--- ExpenseTracker-main/src/Firebase.js
+++ ExpenseTracker-main/src/Firebase.js
@@ -1,20 +1,8 @@
-import { initializeApp } from "firebase/app";
-import {getFirestore} from '@firebase/firestore'
-import {getAuth, GoogleAuthProvider} from 'firebase/auth'
-
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD39hkm2shiHReFowSfRhZNYN_rpw6bQkE",
-  authDomain: "expense-tracker-25fbe.firebaseapp.com",
-  projectId: "expense-tracker-25fbe",
-  storageBucket: "expense-tracker-25fbe.firebasestorage.app",
-  messagingSenderId: "455761208944",
-  appId: "1:455761208944:web:2f4e583f7b8ae8710e76b5"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
-
-const app = initializeApp(firebaseConfig); 
-export const db = getFirestore(app)
-
-export const auth = getAuth(app)
-export const provider = new GoogleAuthProvider()
-
